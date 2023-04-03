bfc2213ba17682e39f5946e488349488885cb0bb
volkaankarakus <vkarakus@etu.edu.tr>
2023-04-03 17:35:56 +0300

    implements OS(file write)

---
+++ 201-IO/file_write.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	//writeOs()
+	createAndWriteFile()
+}
+
+func writeOs() {
+	// ** WriteFile :
+	// 	** ilk parametre : dosya adi
+	//  ** ikinci parametre : dosyanin icerisine yazilacak byteArray
+	// 	** ucuncu parametre : bu dosyayi hangi yetkiyle acmak istiyoruz
+	err := os.WriteFile("testFile.txt", []byte("test"), os.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func createAndWriteFile() {
+	file, error := os.Create("testCreateFileAndWrite.txt")
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	file.Write([]byte("1\n"))
+	file.Write([]byte("2\n"))
+	file.Write([]byte("3\n"))
+	file.Write([]byte("4\n"))
+	file.Write([]byte("5\n"))
+	file.Close()
+}
